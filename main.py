b054130064cec603edf26a530880602ee7d6a5d0
Leo P <leo.projceski@education.nsw.gov.au>
2026-01-12 03:48:59 +0000

    feature: basic menu implmented

---
+++ main.py
@@ -0,0 +1,22 @@
+def menu():
+    cal = 0
+    print(f"Welcome! Your total calorie intake for the day is {cal}.")
+    print("What would you like to input?")
+    print("1. Breakfast\n2. Lunch \n3. Dinner\n4. Snack\n5. Exit")
+    selection = input("Please select the corresponding number: ")
+    if selection == "1":
+        print("Breakfast selected.")
+    elif selection == "2":
+        print("Lunch selected.")
+    elif selection == "3":
+        print("Dinner selected.")
+    elif selection == "4":
+        print("Snack selected.")
+    elif selection == "5":
+        print("Exiting...")
+        exit()
+    else:
+        print("Not a valid input.")
+
+
+menu()
---
+++ meals.py
@@ -0,0 +1,4 @@
+def breakfast():
+    cal = 0
+    intake = int(input("Calorie intake? "))
+    print(f"Your total calories is: {cal + intake}")
