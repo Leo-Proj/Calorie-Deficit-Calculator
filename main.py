1a24280e6f63d38e78d1ad0ce1265f8632c1ea02
Leo P <leo.projceski@education.nsw.gov.au>
2026-01-13 05:43:39 +0000

    wip: input menu for different meal types

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-def menu():
+from meals import Meals
+
+
+def start_menu():
     cal = 0
+    meals = Meals()
     print(f"Welcome! Your total calorie intake for the day is {cal}.")
     print("What would you like to input?")
     print("1. Breakfast\n2. Lunch \n3. Dinner\n4. Snack\n5. Exit")
     selection = input("Please select the corresponding number: ")
     if selection == "1":
         print("Breakfast selected.")
+        meals.Breakfast()
     elif selection == "2":
         print("Lunch selected.")
     elif selection == "3":
@@ -19,4 +24,4 @@ def menu():
         print("Not a valid input.")
 
 
-menu()
+start_menu()
--- meals.py
+++ meals.py
@@ -1,4 +1,30 @@
-def breakfast():
-    cal = 0
-    intake = int(input("Calorie intake? "))
-    print(f"Your total calories is: {cal + intake}")
+cal = 0
+
+
+class Meals:
+    def __init__(self, debug=False):
+        self.__debug = debug
+
+    def Breakfast(self):
+        global cal
+        intake = int(input("Calorie intake? "))
+        cal += intake
+        print(f"Your total calories is: {cal}")
+
+    def Lunch(self):
+        global cal
+        intake = int(input("Calorie intake? "))
+        cal += intake
+        print(f"Your total calories is: {cal}")
+
+    def Dinner(self):
+        global cal
+        intake = int(input("Calorie intake? "))
+        cal += intake
+        print(f"Your total calories is: {cal}")
+
+    def Snack(self):
+        global cal
+        intake = int(input("Calorie intake? "))
+        cal += intake
+        print(f"Your total calories is: {cal}")
